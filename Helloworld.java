be29ebc7a3c22fff9b19163b2ed23bc1eec00e1d
ThrishaMungara <109426727+ThrishaMungara@users.noreply.github.com>
2022-09-29 13:34:42 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+class Helloworld{
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+System.out.println("Enter yiur name");
+int age= in.neextInt();
+if(age<60)
+System.out.println("eligible");
+else
+System.out.println("not eligible");
+}}
+
+
+
+
